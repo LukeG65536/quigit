4af288164b80f34f3c6a7f113c8dbc5143c8bb04
Duffy <116130542+LukeG65536@users.noreply.github.com>
2024-01-28 20:39:11 -0700

    why was the shoe recyler sad
    his job was sould crusing

--- quigit/Assets/Scripts/Physics/PhysicsUpdater.cs
+++ quigit/Assets/Scripts/Physics/PhysicsUpdater.cs
@@ -15,12 +15,12 @@ public class PhysicsUpdater : MonoBehaviour
 
     private void Update()
     {
-        //globalVelocity.y -= gravityStrength * Time.deltaTime;
+        globalVelocity.y -= gravityStrength * Time.deltaTime;
         Vector3 finalVel = globalVelocity * Time.deltaTime;
         finalVel = CollideAndSlide(finalVel, transform.position, 0, false, finalVel);
         globalVelocity = finalVel / Time.deltaTime;
-        globalVelocity = manageGravity(globalVelocity);
-        transform.position += globalVelocity * Time.deltaTime;
+        //globalVelocity = manageGravity(globalVelocity);
+        transform.position += finalVel;
     }
 
     private Vector3 CollideAndSlide(Vector3 vel, Vector3 pos, int depth, bool gravityPass, Vector3 velInit) //stole this from https://www.youtube.com/watch?v=YR6Q7dUz2uk
@@ -28,7 +28,7 @@ public class PhysicsUpdater : MonoBehaviour
         if (depth >= maxBounces)
         { 
             return Vector3.zero;
-        }
+        } 
 
         float dist = vel.magnitude + skinWidth;
 
@@ -38,32 +38,10 @@ public class PhysicsUpdater : MonoBehaviour
         {
             Vector3 snapToSurface = vel.normalized * (hit.distance - skinWidth);
             Vector3 leftover = vel - snapToSurface;
-            float angle = Vector3.Angle(Vector3.up, hit.normal);
 
-            if (snapToSurface.magnitude <= skinWidth)
-            {
-                snapToSurface = Vector3.zero;
-            }
-            if (angle <= 90)
-            {
-                /*if (gravityPass) *///return snapToSurface;
-                leftover = ProjectAndScale(leftover, hit.normal);
-            }
-            else
-            {
-                float scale = 1 - Vector3.Dot(new Vector3(hit.normal.x, 0, hit.normal.z).normalized, -new Vector3(velInit.x, 0, velInit.z).normalized);
-
-                if (true && !gravityPass)
-                {
-                    leftover = ProjectAndScale(new Vector3(leftover.x, 0, leftover.z), new Vector3(hit.normal.x, 0, hit.normal.z));
-                    leftover *= scale;
-                }
-                else
-                {
-                    leftover = ProjectAndScale(leftover, hit.normal) * scale;
-                }
-
-            }
+            float mag = leftover.magnitude;
+            leftover = Vector3.ProjectOnPlane(leftover, hit.normal).normalized;
+            leftover *= mag; 
 
             return snapToSurface + CollideAndSlide(leftover, pos + snapToSurface, depth + 1, gravityPass, velInit);
         }
@@ -77,14 +55,5 @@ public class PhysicsUpdater : MonoBehaviour
         vec = Vector3.ProjectOnPlane(vec, normal).normalized;
         vec *= mag;
         return vec;
-    }
-
-
-    public Vector3 manageGravity(Vector3 vel)
-    {
-        float height = vel.y;
-        float goodHeight = height -= gravityStrength * Time.deltaTime;
-        float convFactor = goodHeight / height;
-        return vel * convFactor;
     }
 }
