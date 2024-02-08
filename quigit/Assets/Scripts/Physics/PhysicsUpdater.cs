95a1d5546be1e52f4600867e7fdb729ba96f35e8
LukeG65536 <116130542+LukeG65536@users.noreply.github.com>
2024-02-08 08:28:44 -0700

    Why did the scarecrow win an award?
    Because he was outstanding in his feild💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀

--- quigit/Assets/Scripts/Physics/PhysicsUpdater.cs
+++ quigit/Assets/Scripts/Physics/PhysicsUpdater.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PhysicsUpdater : MonoBehaviour
@@ -9,65 +10,73 @@ public class PhysicsUpdater : MonoBehaviour
     public Vector3 globalVelocity;
 
     [Header("Physics settings")]
+    public bool xLock = false;
     public float gravityStrength = 10;
     public int maxBounces = 4;
     public float skinWidth = 0.01f;
 
+    public float fpsApprox;
+
+    [Header("refs")]
+    public TextMeshProUGUI fpsText;
+
     private void Update()
     {
-        globalVelocity.y -= gravityStrength * Time.deltaTime;
-        applyPhysics();
+        managePhysics();
 
-        //Vector3 localVelocity = transform.InverseTransformDirection(globalVelocity);
-        //localVelocity.x = 0;
-        //globalVelocity = transform.TransformDirection(localVelocity);
-        //applyPhysics(false);
+        fpsApprox = 1 / Time.smoothDeltaTime;
 
-        //transform.position += globalVelocity * Time.deltaTime;
+        fpsText.text = fpsApprox.ToString();
     }
 
-    public void applyPhysics()
+    private void managePhysics()
     {
-        //globalVelocity.y -= gravityStrength * Time.deltaTime;
+        globalVelocity.y -= gravityStrength * Time.deltaTime;
+        applyPhysics(false);                                       //apply gravity and basic colisions
+
+        if (!xLock) return;                                //if xLocked is false then were done here
+
+        Vector3 localVel = transform.InverseTransformDirection(globalVelocity);    //translate global to local
+        localVel.x = 0;                                                            //set local x vel to 0 for surf
+        globalVelocity = transform.TransformDirection(localVel);                    //translate back to global
+        applyPhysics(true);                                                       //apply phisics but this time preserve vel
+    }
+
+    public void applyPhysics(bool keepVel)
+    {
         Vector3 finalVel = globalVelocity * Time.deltaTime;
-        finalVel = CollideAndSlide(finalVel, transform.position, 0);
+        finalVel = CollideAndSlide(finalVel, transform.position, 0, keepVel);
         transform.position += finalVel;
         globalVelocity = finalVel / Time.deltaTime;
     }
 
-    private Vector3 CollideAndSlide(Vector3 vel, Vector3 pos, int depth) //stole this from https://www.youtube.com/watch?v=YR6Q7dUz2uk
+    private Vector3 CollideAndSlide(Vector3 vel, Vector3 pos, int depth, bool keepVel) //stole this from https://www.youtube.com/watch?v=YR6Q7dUz2uk
     {                                                                                                            // highly recomend watching it
         if (depth >= maxBounces)
         { 
-            return Vector3.zero;
+            return Vector3.zero;      //if max bounces was hit return 0
         } 
 
         float dist = vel.magnitude + skinWidth;
 
         RaycastHit hit;
 
-        if (Physics.SphereCast(pos, 0.5f, vel, out hit, dist))
+        if (Physics.SphereCast(pos, 0.5f, vel, out hit, dist))     //sphere cast of sphere
         {
-            Vector3 snapToSurface = vel.normalized * (hit.distance - skinWidth);
-            Vector3 leftover = vel - snapToSurface;
-            leftover = Vector3.ProjectOnPlane(leftover, hit.normal);
+            Vector3 snapToSurface = vel.normalized * (hit.distance - skinWidth);    // this is the velocity that will move you all the way up to the wall
+            Vector3 leftover = vel - snapToSurface;                                      //whats leftover after the wall snap
+            leftover = Vector3.ProjectOnPlane(leftover, hit.normal);                  //project that onto the plane of the wall
 
+            if (keepVel)                                                  //if snap is on, no velocity is can be lost unless max bounces
+            {
+                leftover = vel - snapToSurface;
+                float mag = leftover.magnitude;  //store length of leftover
+                leftover = Vector3.ProjectOnPlane(leftover, hit.normal).normalized * mag;   //project on plane but have it keep its  vel
+            }
 
-
-            //float mag = leftover.magnitude;
-            //leftover = Vector3.ProjectOnPlane(leftover, hit.normal);
-
-            return snapToSurface + CollideAndSlide(leftover, pos + snapToSurface, depth + 1);
+            return snapToSurface + CollideAndSlide(leftover, pos + snapToSurface, depth + 1, keepVel); //start the function again with the leftover to make sure
         }
 
         return vel;
-    }
-
-    private Vector3 ProjectAndScale(Vector3 vec, Vector3 normal)   //helper functions bellow
-    {
-        float mag = vec.magnitude;
-        vec = Vector3.ProjectOnPlane(vec, normal).normalized;
-        vec *= mag;
-        return vec;
     }
 }
---
+++ quigit/Assets/Scripts/PlayerControllers/RocketLauncher.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketLauncher : MonoBehaviour
+{
+    public bool canShoot = true;
+    public CameraController cameraController;
+    public PhysicsUpdater physics;
+
+    public float blastRadius = 10f;
+
+    public float strength = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canShoot && Input.GetMouseButton(0))
+        {
+            shoot();
+            StartCoroutine(shootCooldown(2f));
+        }
+    }
+
+    private void shoot()
+    {
+        if (Physics.Raycast(cameraController.transform.position, cameraController.transform.forward, out RaycastHit hit, blastRadius))
+        {
+            float dist = hit.distance;
+            Vector3 dir = (cameraController.transform.position - hit.point).normalized;
+            float power = blastRadius - dist;
+
+            dir *= power;
+
+            physics.globalVelocity += dir * strength;
+        }
+    }
+
+    private IEnumerator shootCooldown(float sec)
+    {
+        yield return new WaitForSeconds(sec);
+
+        canShoot = true;
+    }
+}
